92a32bc02be6c288a25a3812fc0c2ed9d6e46441
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2023-01-26 12:24:09 +0600

    create models

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const chats = require("./data/data");
 
 const app = express();
 dotenv.config();
 
+const corsOptions = {
+  origin: ["https://habit-it.chat-app.com", "http://localhost:3000"], //
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  // methods:["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true,
+};
+
+//-> enable cors
+app.use(cors(corsOptions));
+
 app.get("/", (req, res) => {
-    res.send("API is running");
+  res.send("API is running");
 });
 
-app.get("/api/chat", (req, res) => {
-    res.send(chats);
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
 });
 
-app.get("/api/chat/:id", (req, res) => {
-    console.log(req.params.id);
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
+app.get("/api/chats/:id", (req, res) => {
+  console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
 });
 
 const PORT = process.env.PORT || 5000;
