5dcb2be5cdbf3fe3c2165c469a9839dccea19e0d
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2023-01-29 21:29:54 +0600

    register and authenticate a user

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const chats = require("./data/data");
 const connectDB = require("./config/db");
 
+const userRoutes = require("./routes/user");
+
 const app = express();
 dotenv.config();
 connectDB();
@@ -18,19 +20,14 @@ const corsOptions = {
 //-> enable cors
 app.use(cors(corsOptions));
 
+//-> to accept JSON data
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/chats", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chats/:id", (req, res) => {
-  console.log(req.params.id);
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
